69182269ea95637c4b83c93581a91538406686f5
Kymberlee Hill <hillkymberlee@gmail.com>
2015-11-13 02:00:39 -0500

    Create Registration.py

---
+++ Registration.py
@@ -0,0 +1,12 @@
+import requests  
+import JSON
+
+#JSON DICTIONARY
+{
+	"body": {
+		"Email": "hillkymberlee@gmail.com",
+		 "github": "https://github.com/kymwithay/CODE2040"
+		 }
+}
+#Makes the POST REQUEST here, passing body in as the data:
+response = requests.POST("http://challenge.code2040.org/api/register", data = body)
